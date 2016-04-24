d3c79ce4dc147512a1377c2c43cc0d596004985a
= <=>
2016-04-24 10:41:31 -0700

    Added media query. A different image is displayed pending on size of viewport / device.

--- Part1/main.go
+++ Part1/main.go
@@ -1,30 +1,29 @@
 package finalWeb
 
 import (
-        "html/template"
-        "net/http"
+	"html/template"
+	"net/http"
 )
 
 var tpl *template.Template
 
 func init() {
-    configureResourceLocation("css", "css")
-    http.Handle("/favicon.ico", http.NotFoundHandler())                           // ignore favico re quest (error 404)
-    http.HandleFunc("/", index)                                                 // handle main page.
-    tpl = template.Must(template.ParseGlob("html/*.gohtml"))                         // load and parse all web pages for this project.
+	configureResourceLocation("css", "css")
+	configureResourceLocation("images", "img")
+	http.Handle("/favicon.ico", http.NotFoundHandler())      // ignore favico re quest (error 404)
+	http.HandleFunc("/", index)                              // handle main page.
+	tpl = template.Must(template.ParseGlob("html/*.gohtml")) // load and parse all web pages for this project.
 }
 
 // map resource physical location ro href relative location.
 // phyDir : resource files physical location relative to html file.
 // hrefDir: resource location as defined withing the href tag.
 func configureResourceLocation(phyDir, hrefDir string) {
-    fs := http.FileServer(http.Dir(phyDir))
-    fs = http.StripPrefix("/"+hrefDir, fs)
-    http.Handle("/"+hrefDir+"/", fs)
+	fs := http.FileServer(http.Dir(phyDir))
+	fs = http.StripPrefix("/"+hrefDir, fs)
+	http.Handle("/"+hrefDir+"/", fs)
 }
 
 func index(res http.ResponseWriter, req *http.Request) {
-    tpl.ExecuteTemplate(res, "index.gohtml", nil)                               // executer main page HTML template
+	tpl.ExecuteTemplate(res, "index.gohtml", nil) // executer main page HTML template
 }
-
-    
